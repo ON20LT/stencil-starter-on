fdba5d036a45edaf53faa96ed953420d8f172cc6
ON20-LowisSpross <lowis.spross@gmail.com>
2021-04-21 16:29:05 +0200

    worked on newsletter registration

--- src/components/ls-newsletter/ls-newsletter.tsx
+++ src/components/ls-newsletter/ls-newsletter.tsx
@@ -25,17 +25,22 @@ export class Newsletter {
   
   render() {
       return (
-           
-             <div>
-             <h2>Melde dich jetzt zu unserem Newsletter an</h2>
-                  <p>LOREM IPSUM LOREM IPSUM LOREM IPSUM LOREM IPSUM LOREM IPSUM</p>
-              <form onClick={(event) => this.byClick(event)}>
-                    <label>
-                    E-mail Adresse
-                      <input type="text" value={this.email} onInput={(e) => this.forChange(e)}/>
-                    </label>
-                    <button type="click" value="Click">Jetzt anmelden</button>
-              </form>
+        
+             <div class="wrapper">
+                <img src="src/components/ls-newsletter/assets/beach-1761410_1920.jpg"></img>
+                <image id="overlay">
+                    <div id="content">
+                    <span>
+                        <h2>Zum Newsletter anmelden</h2>
+                    </span>
+                    <form onClick={(event) => this.byClick(event)}>
+                        <label class="email-label">E-mail Adresse <br/>
+                          <input class="email-input" type="text" placeholder="example@onbnb.de"/>
+                        </label>
+                    </form>
+                    <button id="submit-button">Jetzt anmelden</button>
+                    </div>
+                </image> 
               </div>
             
         )
