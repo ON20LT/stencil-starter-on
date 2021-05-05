07d55c282f94a2a19ef22b5f81611827c6c5db0a
Nina <ninakrauss@rocketmail.com>
2021-05-05 10:44:37 +0200

    click function flipcard

--- src/components/nk-flipcard/nk-flipcard.tsx
+++ src/components/nk-flipcard/nk-flipcard.tsx
@@ -9,6 +9,10 @@ export class NkFlipcard{
 
   @Prop({reflect: true, mutable: true}) imgSrc: string;
 
+  private handleClick = () => {
+    alert("Vielen Dank für Dein Interesse!")
+  };
+
   render() {
     return (
       <Host>
@@ -22,7 +26,7 @@ export class NkFlipcard{
             </div>
             <div class="card-back">
               <slot name="card-back">Lust auf Meer? Finde Deinen Sommertraum bei ONbnb! </slot>
-              <button id="pic1">Jetzt buchen</button>
+              <button id="pic1" onClick={this.handleClick}>Jetzt buchen</button>
             </div>
             </div>
 
@@ -32,7 +36,7 @@ export class NkFlipcard{
             </div>
             <div class="card-back2">
               <slot name="card-back2"></slot>
-              <button id="pic2">Jetzt buchen</button>
+              <button id="pic2" onClick={this.handleClick}>Jetzt buchen</button>
             </div>
             </div>
 
@@ -42,7 +46,7 @@ export class NkFlipcard{
             </div>
             <div class="card-back3">
               <slot name="card-back3"></slot>
-              <button id="pic3">Jetzt buchen</button>
+              <button id="pic3" onClick={this.handleClick}>Jetzt buchen</button>
             </div>
             </div>
             </div>
