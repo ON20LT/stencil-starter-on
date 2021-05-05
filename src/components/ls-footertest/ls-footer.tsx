dc6378fbc87ac54a4a1c817862f5e7fd5dc2dee8
ON20-LowisSpross <lowis.spross@gmail.com>
2021-05-05 16:59:52 +0200

    changed added type

--- src/components/ls-footertest/ls-footer.tsx
+++ src/components/ls-footertest/ls-footer.tsx
@@ -12,7 +12,7 @@ export class LsFootertest {
 
   @Prop({reflect:true, mutable:true})
 
-  getFooterLinks(): HTMLLIElement[]{//mit Fremdcode
+  getFooterLinks(): HTMLLinkElement[]{//mit Fremdcode
     const allLinks = [];
 
     const slotElement = this.element.querySelector('[slot="footer-links"]');
